ab97d7195c70836aa2cd98ec332839f989b584cb
stevenbarba <stevenbarba@gmail.com>
2023-01-20 07:47:26 -0800

    Fixed version # in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='cleartest',
-    version='0.923',
+    version='0.924444',
     description='Lightweight testing framework for Python 3',
     long_description="See the project's GitHub page for docs: https://github.com/sbarba/cleartest",
     py_modules=['cleartest'],
